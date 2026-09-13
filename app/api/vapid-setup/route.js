563b3342010bfc0ecddb0ec2b9d86d1bc3ed839e
linuxer80-cmd <linuxer80@gmail.com>
2026-09-13 15:07:40 +0900

    Update route.js

--- app/api/vapid-setup/route.js
+++ app/api/vapid-setup/route.js
@@ -1,1 +1,10 @@
+import webpush from "web-push";
 
+export async function GET() {
+  const keys = webpush.generateVAPIDKeys();
+
+  return Response.json({
+    publicKey: keys.publicKey,
+    privateKey: keys.privateKey,
+  });
+}
