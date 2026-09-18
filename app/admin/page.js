a98c93b5b1c3593ea22f377fc3db42f442014a29
linuxer80-cmd <linuxer80@gmail.com>
2026-09-18 12:19:51 +0900

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,6 +1,7 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react"
+import { supabase } from "../../lib/supabase";
 import {
   formatDate,
   formatWon,
