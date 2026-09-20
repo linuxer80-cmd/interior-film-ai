d197600feb99bbfdbb2fec3444cd41f1e205da28
linuxer80-cmd <linuxer80@gmail.com>
2026-09-21 03:48:47 +0900

    Update page.js

--- app/samples/page.js
+++ app/samples/page.js
@@ -1,7 +1,7 @@
 "use client";
-
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
 import { supabase } from "../../lib/supabase";
 
 const PAGE_SIZE = 16;
